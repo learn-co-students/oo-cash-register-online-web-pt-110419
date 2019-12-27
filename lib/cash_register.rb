a27f65ae3f3a83fecb6017d115fe3760464915e7
Oksana Nichols <oksanahnichols@yahoo.com>
2019-12-27 09:07:42 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class Person 
+  
+  attr_accessor :age 
+  
+  def initialize(age = 0)
+  @age = age 
+end 
 
+def birthday 
+  self.age += 1 
+end 
+end 
+
+describe 'CashRegister' do
+  let(:cash_register) { CashRegister.new }
+  let(:cash_register_with_discount) { CashRegister.new(20) }
